44773a59e80771cb30c31f0d4437220ec0394a80
Kunal <87492428+boaluma@users.noreply.github.com>
2023-10-29 23:21:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,11 +34,16 @@ with st.sidebar:
 
 # Function to extract text from a PDF file
 def extract_text_from_pdf(pdf_path):
-    doc = fitz.open(pdf_path)
+    # doc = fitz.open(pdf_path)
+    # text = ''
+    # for page_num in range(doc.page_count):
+    #     page = doc.load_page(page_num)
+    #     text += page.get_text()
+    # return text
+    pdf_reader = PdfReader(pdf_path)
     text = ''
-    for page_num in range(doc.page_count):
-        page = doc.load_page(page_num)
-        text += page.get_text()
+    for page in pdf_reader:
+        text += page.extract_text()
     return text
 
 # Initialize previous conversation
